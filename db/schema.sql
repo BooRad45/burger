fbc36d182e2684506725d94845c0f607ea48fe3b
Brendan Rogers <brendan@utexas.edu>
2017-04-28 23:37:32 -0500

    changed script in json file

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
-CREATE TABLE burgers(
-	id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE burgers (
+	id INT AUTO_INCREMENT NOT NULL ,
 	burger_name VARCHAR(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-	date TIMESTAMP,
+	createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 	);
 
